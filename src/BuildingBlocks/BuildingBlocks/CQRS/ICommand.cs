c70a9d0fa50fd8bc540e08c6b486bc19b9b4e1f0
ahmedlotfi <ahmedlotfiali@gmail.com>
2024-11-12 06:15:31 +0400

    update interfaces generic

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -2,12 +2,10 @@ using MediatR;
 
 namespace BuildingBlocks.CQRS;
 
-public interface ICommand<out TResponse>:IRequest<TResponse>
+public interface ICommand<out TResponse> : IRequest<TResponse>
 {
-    
 }
 
 public interface ICommand : ICommand<Unit>
 {
-    
 }
