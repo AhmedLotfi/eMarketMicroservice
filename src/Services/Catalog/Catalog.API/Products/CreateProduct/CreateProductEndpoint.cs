739110d179b4b2f2b8bf80e12f7536762da88cfa
ahmedlotfi <ahmedlotfiali@gmail.com>
2024-11-23 21:53:06 +0400

    http file, create product endpoint.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,4 @@
-using Carter;
-using Mapster;
-using MediatR;
+
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -18,6 +16,11 @@ public class CreateProductEndpoint:ICarterModule
             var response = result.Adapt<CreateProductResponse>();
 
             return Results.Created($"/products/{response.Id}",response);
-        });
+        })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
     }
 }
